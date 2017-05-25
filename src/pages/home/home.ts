7c418c2b0e5e9c283fc111393c94a81a2355d7c6
steve <sts738@mun.ca>
2017-05-25 00:55:56 -0230

    added some dummy data for now

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,7 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public cards: Array<any> = [];
+
   constructor(public navCtrl: NavController) {
+
+    this.cards.push({ name: 'Plant Type', value: 'Tomato' });
+    this.cards.push({ name: 'Soil Moisture', value: '30%' });
+    this.cards.push({ name: 'Exhaust Fan Speed', value: '1200RPM' });
+    this.cards.push({ name: 'Intake Fan Speed', value: '1200RPM' });
+    this.cards.push({ name: 'Air Temperature', value: '21C' });
+    this.cards.push({ name: 'Soil PH', value: '7.5' });
 
   }
 
